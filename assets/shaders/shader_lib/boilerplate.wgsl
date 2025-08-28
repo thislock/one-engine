151ab5c2e3e88c9ce8276ccee0518061b83d2ed4
thislock <callmemaster4444@gmail.com>
2025-08-28 01:27:29 -0400

    added some evil shader library support

---
+++ assets/shaders/shader_lib/boilerplate.wgsl
@@ -0,0 +1,25 @@
+
+struct CameraUniform {
+    view_proj: mat4x4<f32>,
+};
+@group(1) @binding(0) // 1.
+var<uniform> camera: CameraUniform;
+
+struct GpuTime {
+    time_secs: f32,
+};
+
+@group(2) @binding(0)
+var<uniform> time: GpuTime;
+
+struct VertexInput {
+    @location(0) position: vec3f,
+    @location(1) tex_coords: vec2f,
+    @location(2) normal: vec3f,
+}
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4f,
+    @location(0) tex_coords: vec2f,
+    @location(1) normal: vec3f,
+};
--- paper/src/files.rs
+++ paper/src/files.rs
@@ -8,6 +8,7 @@ pub enum FileType {
   Image,
   Obj,
   Shader,
+  ShaderLib,
 }
 
 fn bytes_to_str(bytes: Vec<u8>) -> io::Result<String> {
@@ -21,8 +22,52 @@ fn load_file_string(filetype: FileType, filename: &str) -> io::Result<String> {
 }
 
 // ********************** SHADERS **************************** //
+
+/// i know loading every shader file in the lib folder every time is slow,
+/// but shut up it doesn't REALLY matter for this kinda thing
+fn load_lib_files() -> io::Result<Vec<String>> {
+  let lib_path = &get_path(FileType::ShaderLib);
+  fs::create_dir_all(Path::new(&lib_path))?;
+  let entries = fs::read_dir(lib_path)?;
+
+  let mut lib_files = Vec::new();
+
+  for entry in entries {
+    if let Ok(valid_entry) = entry {
+      let found_file = &valid_entry.file_name();
+      let filename = std::ffi::OsStr::to_str(found_file);
+      if let Some(valid_filename) = filename {
+        let file_str = load_file_string(FileType::ShaderLib, valid_filename)?;
+        lib_files.push(file_str);
+      }
+    }
+  }
+
+  return Ok(lib_files);
+}
+
+fn format_shader_libraries() -> io::Result<String> {
+  let shader_libs = load_lib_files()?;
+  let mut formated_shaders = String::new();
+
+  for shader in shader_libs {
+    formated_shaders.push_str(&shader);
+    formated_shaders.push('\n');
+    formated_shaders.push_str("// hi");
+    formated_shaders.push('\n');
+  }
+
+  return Ok(formated_shaders);
+}
+
 pub fn load_shader_str(filename: &str) -> io::Result<String> {
-  return Ok(load_file_string(FileType::Shader, filename)?);
+  let shader_libraries = &format_shader_libraries()?;
+  let shader_data = &load_file_string(FileType::Shader, filename)?;
+  let mut shader = String::new();
+  // literally just dumps everything in the shader lib folder at the front of the shader file
+  shader.push_str(shader_libraries);
+  shader.push_str(shader_data);
+  return Ok(shader);
 }
 
 // ********************** OBJ FILES **************************** //
@@ -56,16 +101,32 @@ fn ensure_directory_exists(path: &Path) -> io::Result<()> {
   Ok(())
 }
 
-fn get_file_path(filetype: FileType, filename: &str) -> String {
+mod folder_names {
+  pub const IMAGES:     &'static str = "images";
+  pub const OBJECTS:    &'static str = "obj";
+  pub const SHADERS:    &'static str = "shaders";
+  pub const SHADER_LIB: &'static str = "shader_lib";
+}
+
+fn get_path(filetype: FileType) -> String {
   let mut path = String::new();
   path.push_str(DEFAULT_PATH);
   match filetype {
-    FileType::Image => add_directory(&mut path, "images"),
-    FileType::Obj => add_directory(&mut path, "obj"),
-    FileType::Shader => add_directory(&mut path, "shaders"),
+    FileType::Image => add_directory(&mut path, folder_names::IMAGES),
+    FileType::Obj => add_directory(&mut path, folder_names::OBJECTS),
+    FileType::Shader => add_directory(&mut path, folder_names::SHADERS),
+    FileType::ShaderLib => {
+      add_directory(&mut path, folder_names::SHADERS);
+      add_directory(&mut path, folder_names::SHADER_LIB);
+    },
   }
+  return path;
+}
+
+fn get_file_path(filetype: FileType, filename: &str) -> String {
+  let mut path = get_path(filetype);
   add_directory(&mut path, filename);
-  path
+  return path;
 }
 
 fn read_file_as_raw(path: &Path) -> io::Result<Vec<u8>> {
