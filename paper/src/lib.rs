e6e277d581887817b090600ef42dc1795e620108
thislock <callmemaster4444@gmail.com>
2025-09-01 16:37:58 -0400

    started work on decoupling the engine calls

--- paper/src/lib.rs
+++ paper/src/lib.rs
@@ -5,19 +5,18 @@ use sdl3::{
 };
 
 use crate::{
-  gpu::object::{ObjectBuilder},
   window::{sdl_handle::SdlHandle, tickrate, user_input::MovementHandler},
 };
 
-mod files;
-mod maths;
+pub mod files;
+pub mod maths;
 
 #[path = "1engine.rs"]
-mod engine;
+pub mod engine;
 
-mod gpu;
-mod tasks;
-mod window;
+pub mod gpu;
+pub mod tasks;
+pub mod window;
 
 fn handle_system_events(
   event: &sdl3::event::Event,
@@ -39,52 +38,45 @@ fn handle_system_events(
   }
 }
 
-async fn init_objects(e: &mut engine::Engine) -> anyhow::Result<()> {
-  e.texture_bundle
-    .add_texture_from_file(&e.drivers, "detail.png", "test")?;
-
-  let diffuse = e.texture_bundle.get_texture_bind("test");
-
-  let object = ObjectBuilder::new()
-    .load_meshes_from_objfile(&e.texture_bundle, &e.drivers, "test.obj")?
-    .add_diffuse_texture(diffuse.clone())
-    .build();
-
-  e.render_task
-    .add_object(object, &e.drivers, &e.data_bindgroups)
-    .await?;
-
-  Ok(())
+pub struct EngineRuntime {
+  sdl_handle: SdlHandle,
+  pub engine: engine::Engine,
 }
 
-pub async fn run_engine() -> anyhow::Result<()> {
-  let mut sdl_handle = SdlHandle::new()?;
-  let mut engine = engine::Engine::new(&sdl_handle, sdl_handle.sdl_window.clone()).await;
+impl EngineRuntime {
+  pub async fn new_engine() -> anyhow::Result<Self> {
+    let sdl_handle = SdlHandle::new()?;
+    let engine = engine::Engine::new(&sdl_handle, sdl_handle.sdl_window.clone()).await;
 
-  let mut movement_buffer = vec![];
-  let mut sys_window = sdl_handle.sdl_window.clone();
+    let new_engine = Self { sdl_handle, engine };
 
-  let mut benchmark = tickrate::TimeMeasurer::new();
-
-  init_objects(&mut engine).await?;
-
-  while engine.is_running() {
-    benchmark.start_measure();
-
-    movement_buffer.clear();
-
-    for event in sdl_handle.event_pump.poll_iter() {
-      handle_system_events(&event, &mut sys_window, &mut engine);
-      MovementHandler::poll_movement(&mut engine, &mut movement_buffer, &event);
-    }
-    MovementHandler::apply_movement(&mut engine, &mut movement_buffer);
-
-    engine.tickrate.tick();
-    engine.redraw();
-    benchmark.stop_measure();
-    //println!("{}", benchmark.get_average());
-    engine.tickrate.sleep_until_next_frame();
+    return Ok(new_engine);
   }
 
-  Ok(())
+  pub async fn run_engine(mut self) -> anyhow::Result<()> {
+    let mut movement_buffer = vec![];
+    let mut sys_window = self.sdl_handle.sdl_window.clone();
+
+    let mut benchmark = tickrate::TimeMeasurer::new();
+
+    while self.engine.is_running() {
+      benchmark.start_measure();
+
+      movement_buffer.clear();
+
+      for event in self.sdl_handle.event_pump.poll_iter() {
+        handle_system_events(&event, &mut sys_window, &mut self.engine);
+        MovementHandler::poll_movement(&mut self.engine, &mut movement_buffer, &event);
+      }
+      MovementHandler::apply_movement(&mut self.engine, &mut movement_buffer);
+
+      self.engine.tickrate.tick();
+      self.engine.redraw();
+      benchmark.stop_measure();
+      //println!("{}", benchmark.get_average());
+      self.engine.tickrate.sleep_until_next_frame();
+    }
+
+    Ok(())
+  }
 }
--- person/src/main.rs
+++ person/src/main.rs
@@ -1,6 +1,28 @@
-use paper::run_engine;
+use paper::{engine, gpu::object::{ObjectBuilder}, EngineRuntime};
+
+async fn init_objects(e: &mut engine::Engine) -> anyhow::Result<()> {
+  e.texture_bundle
+    .add_texture_from_file(&e.drivers, "detail.png", "test")?;
+
+  let diffuse = e.texture_bundle.get_texture_bind("test");
+
+  let object = ObjectBuilder::new()
+    .load_meshes_from_objfile(&e.texture_bundle, &e.drivers, "test.obj")?
+    .add_diffuse_texture(diffuse.clone())
+    .build();
+
+  e.render_task
+    .add_object(object, &e.drivers, &e.data_bindgroups)
+    .await?;
+
+  Ok(())
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-  return run_engine().await;
+  let mut engine = EngineRuntime::new_engine().await?;
+
+  init_objects(&mut engine.engine).await?;
+
+  return engine.run_engine().await;
 }
